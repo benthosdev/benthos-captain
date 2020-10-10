d367ac5744574076e50bec803d66358275d9510b
Marco Amador <amador.marco@gmail.com>
2020-10-10 17:30:09 +0100

    Updated version

--- api/v1beta1/groupversion_info.go
+++ api/v1beta1/groupversion_info.go
@@ -26,7 +26,7 @@ import (
 
 var (
 	// GroupVersion is group version used to register these objects
-	GroupVersion = schema.GroupVersion{Group: "benthos.dev.my.domain", Version: "v1"}
+	GroupVersion = schema.GroupVersion{Group: "benthos.dev", Version: "v1beta1"}
 
 	// SchemeBuilder is used to add go types to the GroupVersionKind scheme
 	SchemeBuilder = &scheme.Builder{GroupVersion: GroupVersion}
