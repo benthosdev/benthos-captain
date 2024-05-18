76ced78871a208e1a9925fc3eae69e3d611aca26
Mike Baum <michael.baum@gmail.com>
2024-05-18 11:12:14 -0400

    add ImagePullSecret property for setting an image pull secret if needed (#21)
    Signed-off-by: Mike Baum <michael.baum@gmail.com>

--- internal/pkg/resource/resource.go
+++ internal/pkg/resource/resource.go
@@ -20,6 +20,48 @@ func NewDeployment(name string, namespace string, scope captainv1.PipelineSpec) 
 		image = scope.Image
 	}
 
+	podSpec := corev1.PodSpec{
+		Containers: []corev1.Container{{
+			Image:           image,
+			ImagePullPolicy: corev1.PullAlways,
+			Name:            "benthos",
+			Ports: []corev1.ContainerPort{{
+				ContainerPort: 4195,
+				Name:          "http",
+			}},
+			Args: []string{
+				"-c",
+				"/config/benthos.yaml",
+			},
+			VolumeMounts: []corev1.VolumeMount{
+				{
+					Name:      "config",
+					MountPath: "/config",
+					ReadOnly:  true,
+				},
+			},
+			Env: scope.Env,
+		}},
+		Volumes: []corev1.Volume{
+			{
+				Name: "config",
+				VolumeSource: corev1.VolumeSource{
+					ConfigMap: &corev1.ConfigMapVolumeSource{
+						LocalObjectReference: corev1.LocalObjectReference{
+							Name: "benthos-" + name,
+						},
+					},
+				},
+			},
+		},
+	}
+
+	if scope.ImagePullSecret != "" {
+		podSpec.ImagePullSecrets = []corev1.LocalObjectReference{
+			{Name: scope.ImagePullSecret},
+		}
+	}
+
 	return &appsv1.Deployment{
 		ObjectMeta: metav1.ObjectMeta{
 			Name:      name,
@@ -34,41 +76,7 @@ func NewDeployment(name string, namespace string, scope captainv1.PipelineSpec) 
 				ObjectMeta: metav1.ObjectMeta{
 					Labels: labels,
 				},
-				Spec: corev1.PodSpec{
-					Containers: []corev1.Container{{
-						Image:           image,
-						ImagePullPolicy: corev1.PullAlways,
-						Name:            "benthos",
-						Ports: []corev1.ContainerPort{{
-							ContainerPort: 4195,
-							Name:          "http",
-						}},
-						Args: []string{
-							"-c",
-							"/config/benthos.yaml",
-						},
-						VolumeMounts: []corev1.VolumeMount{
-							{
-								Name:      "config",
-								MountPath: "/config",
-								ReadOnly:  true,
-							},
-						},
-						Env: scope.Env,
-					}},
-					Volumes: []corev1.Volume{
-						{
-							Name: "config",
-							VolumeSource: corev1.VolumeSource{
-								ConfigMap: &corev1.ConfigMapVolumeSource{
-									LocalObjectReference: corev1.LocalObjectReference{
-										Name: "benthos-" + name,
-									},
-								},
-							},
-						},
-					},
-				},
+				Spec: podSpec,
 			},
 		},
 	}
